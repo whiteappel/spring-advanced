b6a0f139d3a79276e750745bfa49df0da86dcd49
조효준 <148june@naver.com>
2025-05-01 18:50:49 +0900

    feat: 레벨 3 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.todo.repository;
 
+import jakarta.persistence.Entity;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.EntityGraph;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 /*기존꺼 fetch join 기반의 n+1 해결하고 있다.
@@ -37,5 +39,18 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
+    @EntityGraph
+    @Query("SELECT t FROM Todo t " +
+            "WHERE (:weather is null or t.weather= :weather) " +
+            "AND (:startAt is null or t.modifiedAt >= :startAt) " +
+            "AND (:endAt is null or t.modifiedAt <= :endAt) " +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findtodobyweather(
+            @Param("weather") String weather,
+            @Param("startAt") LocalDateTime startAt,
+            @Param("endAt") LocalDateTime endAt,
+            Pageable pageable
+            );
+
 }
 
